e73c49e84eff7ba75b2a8c0810b9c27fbf5d827e
gbs4ever <gbs4ever2@gmail.com>
2019-09-24 13:19:44 -0400

    change to password digest

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_09_18_221539) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
