8244e2b39903e4a22154690f55832378dc8d4354
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-28 17:11:39 -0400

    beautified file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
-# we need to change this secret into env file 
-@mysecret ="myapp"
-    def self.create_token(user_object) 
+# we need to change this secret into env file
+  @mysecret ="myapp"
 
-      payload = { user: JSON.parse(user_object.to_json) }
-        token = JWT.encode(payload, @mysecret, 'HS256')
+  def self.create_token(user_object)
+    payload = { user: JSON.parse(user_object.to_json) }
+    token = JWT.encode(payload, @mysecret, 'HS256')
+  end
 
-    end
-    def self.decode_token(token) 
+  def self.decode_token(token)
     decoded = JWT.decode(token, @mysecret, true, { algorithm: 'HS256' })
-     end
-
-
-
-
-
+  end
 end
