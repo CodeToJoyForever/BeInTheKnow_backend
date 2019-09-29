546711730812c6cacc94a6c3ec20c826cba61c1a
gbs4ever <gbs4ever2@gmail.com>
2019-09-28 21:36:19 -0400

    fixed indentation and removed destroy method sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,20 +4,15 @@ class Api::SessionsController < ApplicationController
       welcome: "your API has loaded"
     }
   end
-    def create
-  
-       @user = User.find_by(username: params[:username])
-     if @user && @user.authenticate(params[:password])
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
       render json: {message: "You have been authenticated #{@user.username} ",token: User.create_token(@user) }
     else 
         render json:{
           errors: "Your Username and Password do not match, please try again"
       } 
-      end  
-   end
-  
-#with tokens we dont need to clear sessions on the server just clear token from front-end
-    # def destroy
-  
-    # end
-  end  
+    end  
+  end
+     
+end  
