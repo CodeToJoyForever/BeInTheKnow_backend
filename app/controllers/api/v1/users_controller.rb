4bd78cb3b0ac0763d1ab853edf0fa6487bb33d00
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-29 07:46:19 -0400

    fix indentation.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,19 +6,19 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-     #verify_token(@user)
+    #verify_token(@user)
     #render json: @user
   end
 
   # POST /users
   def create
-
     @user = User.new(email: params[:email], password: params[:password])
+
     if @user.save
       render json: {message: "You have been authenticated #{@user.username} ",token: User.create_token(@user) }
     else
       render json:{
-        errors: @user.errors.full_messages } #, status: "Please enter a valid email and password!"
+      errors: @user.errors.full_messages } #, status: "Please enter a valid email and password!"
     end
   end
 
@@ -27,11 +27,14 @@ class Api::V1::UsersController < ApplicationController
     @user.destroy
   end
 
-  private
 
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:email,:username, :password)
-    end
+private
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:email,:username, :password)
+  end
+
+
 end
